c9a233e51f7537f878a2b08dc5c920f00f28b2d4
Alexey Dolgov <alekseydolgov17@mail.ru>
2023-03-25 11:57:52 +0300

    2 миграции
    Signed-off-by: Alexey Dolgov <alekseydolgov17@mail.ru>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('name', 100);
+            $table->text('email');
+            $table->text('password');
+            $table->boolean('status_prav')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2023_03_25_075334_profile.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->nullable(false);
+            $table->text('user_photo');
+            $table->string('about_me')->nullable();
+            $table->date('last_vist')->nullable();
+            $table->text('photo_technic')->nullable();
+            $table->text('place_residence')->nullable();
+            $table->date('date_regiatration');
+            $table->string('last_name', 100);
+            $table->string('patronymic', 100)->nullable();
+            $table->foreign('user_id')
+                ->references('id')->on('users');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
