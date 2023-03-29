c49823662955214428ae7309c3e5f7e788f88b3e
AlekseyDolgov <alekseydolgov17@mail.ru>
2023-03-29 15:56:14 +0300

    Добавление layouts и ThreadsController -- сновные шаблоны сайта и страницу для списка тем
    Signed-off-by: AlekseyDolgov <alekseydolgov17@mail.ru>

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ThreadsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', [ThreadsController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
