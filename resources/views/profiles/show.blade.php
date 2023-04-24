2dab9373f49632a220e5f3d06f14ffc7b81263a4
AlekseyDolgov <alekseydolgov17@mail.ru>
2023-04-24 22:45:15 +0300

    небольшие добавления на пользовательскую страницу. -- Добавлена аватарка по умолчанию -- доп. столбцы
    Signed-off-by: AlekseyDolgov <alekseydolgov17@mail.ru>

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.site')
 
-{{--<h3>user-id: {{$profile->id}}</h3>--}}
-
-
 {{--<p>Имя: {{ $user->name }}</p>--}}
 {{--<p>Почта: {{ $user->email }}</p>--}}
 {{--<img src="{{ asset('storage/' . $user->user_photo)}}" style="max-width: 100%; height: 125px; margin: 20px 0;">--}}
@@ -14,20 +11,22 @@
 {{--<p>{{ $user->last_name }}</p>--}}
 {{--<p>{{ $user->patronymic }}</p>--}}
 @section('content')
-<h1>страница пользователя</h1>
+
 <div class="container mt-5">
     <div class="row">
         <div class="col-md-6">
-            <h1>{{ $user->name }}</h1>
-            <p>{{ $user->email }}</p>
-            <p>{{ $user->last_name }} {{ $user->patronymic }}</p>
-            <p>{{ $user->place_residence }}</p>
-            <p>{{ $user->last_vist }}</p>
+            <img src="{{ asset('storage/' . $user->user_photo) }}" style="max-width: 100%; height: 40%; margin: 20px 0;" class="img-fluid rounded">
+            <h1>имя: {{ $user->name }}</h1>
+            <p>фамилия: {{ $user->last_name }} отчество: {{ $user->patronymic }}</p>
+            <p class="mt-3">статус: {{ $user->about_me }}</p>
+
         </div>
         <div class="col-md-6">
-            <img src="{{ asset('storage/' . $user->user_photo)}}" style="max-width: 100%; height: 125px; margin: 20px 0;" class="img-fluid rounded">
-            <p class="mt-3">{{ $user->about_me }}</p>
-            <p>{{ $user->photo_technic }}</p>
+            <p>фото-техника: {{ $user->photo_technic }}</p>
+            <p>почта: {{ $user->email }}</p>
+            <p>место проживания: {{ $user->place_residence }}</p>
+            <p>дата рождения: {{ $user->last_vist }}</p>
+            <a href="#">статистика</a>
         </div>
     </div>
 </div>
